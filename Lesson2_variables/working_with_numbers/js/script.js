0266e63071d0c14498f5000c849823736b2c8a27
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-11-16 17:32:47 +0200

    my js lessons

---
+++ Lesson2_variables/working_with_numbers/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+let a = 4;
+let b = 2;
+console.log('a:', a);
+console.log('b:', b);
+console.log('a - b =', a - b);
+console.log('a + b =', +a + +b);
+console.log('a / b =', a / b);
+console.log('a * b =', a * b);
