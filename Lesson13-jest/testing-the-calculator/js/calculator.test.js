0b7639819e5f97a28a6f907b06c634f0ba577785
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-11-25 01:17:38 +0200

    task4

---
+++ Lesson13-jest/testing-the-calculator/js/calculator.test.js
@@ -0,0 +1,26 @@
+import { calc } from './calculator';
+
+it('calc should plus numbers', () => {
+  const result = calc('1 + 3');
+  expect(result).toEqual('1 + 3 = 4');
+});
+
+it('calc should multiply numbers', () => {
+  const result = calc('2 * 3');
+  expect(result).toEqual('2 * 3 = 6');
+});
+
+it('calc should divide numbers', () => {
+  const result = calc('10 / 2');
+  expect(result).toEqual('10 / 2 = 5');
+});
+
+it('calc should subtract numbers', () => {
+  const result = calc('5 - 2');
+  expect(result).toEqual('5 - 2 = 3');
+});
+
+it('calc should return null if not a string', () => {
+  const result = calc(5);
+  expect(result).toEqual(null);
+});
