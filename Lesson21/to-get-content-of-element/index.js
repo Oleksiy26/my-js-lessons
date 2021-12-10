3dd44293272d09e1654815b0dab1289841e7895f
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-10 22:22:35 +0200

    add solved

--- Lesson21/to-get-content-of-element/index.js
+++ Lesson21/to-get-content-of-element/index.js
@@ -1,22 +1,22 @@
-const getTitle = () => {
+export const getTitle = () => {
   const titleEl = document.querySelector('.title');
   return titleEl.textContent;
 };
 console.log(getTitle());
 
-const getDescription = () => {
+export const getDescription = () => {
   const paragraphEl = document.querySelector('.about');
   return paragraphEl.innerText;
 };
 console.log(getDescription());
 
-const getPlans = () => {
+export const getPlans = () => {
   const paragraphEl = document.querySelector('.plans');
   return paragraphEl.innerText;
 };
 console.log(getPlans());
 
-const getGoal = () => {
+export const getGoal = () => {
   const paragraphEl = document.querySelector('.goal');
   return paragraphEl.outerText;
 };
