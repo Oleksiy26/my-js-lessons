9be19ca5375abdfa8854f28e991deba54a5989cd
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-11 01:27:52 +0200

    add solved

--- Lesson21/create-the-DOM-elements-and-add-it-to-page-1/js/script.js
+++ Lesson21/create-the-DOM-elements-and-add-it-to-page-1/js/script.js
@@ -1,18 +1,7 @@
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-  set fullName(value) {
-    const [firstName, lastName] = value.split(' ');
-    this.firstName = firstName;
-    this.lastName = lastName;
-  },
+const createButton = buttonText => {
+  const myButton = document.createElement('button');
+  myButton.textContent = buttonText;
+  document.body.append(myButton);
 };
 
-export default user;
-
-user.fullName = 'bob jokovich';
-
-console.log(user.firstName);
+// createButton('Send Email');
