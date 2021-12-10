2678022f7f2d0a0ea20de52d9ef64bd0250de290
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-11 00:20:34 +0200

    first try

--- Lesson21/to-get-content-of-element/index.js
+++ Lesson21/to-get-content-of-element/index.js
@@ -2,22 +2,22 @@ export const getTitle = () => {
   const titleEl = document.querySelector('.title');
   return titleEl.textContent;
 };
-console.log(getTitle());
+// console.log(getTitle());
 
 export const getDescription = () => {
   const paragraphEl = document.querySelector('.about');
   return paragraphEl.innerText;
 };
-console.log(getDescription());
+// console.log(getDescription());
 
 export const getPlans = () => {
   const paragraphEl = document.querySelector('.plans');
   return paragraphEl.innerText;
 };
-console.log(getPlans());
+// console.log(getPlans());
 
 export const getGoal = () => {
   const paragraphEl = document.querySelector('.goal');
   return paragraphEl.outerText;
 };
-console.log(getGoal());
+// console.log(getGoal());
