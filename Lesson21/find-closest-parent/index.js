f877cbb8679d063d717f75b9da18152dc7a5532e
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-11 21:04:53 +0200

    add solved

--- Lesson21/find-closest-parent/index.js
+++ Lesson21/find-closest-parent/index.js
@@ -1,8 +1,14 @@
+// export const getSection = num => {
+//   const mySpan = document.querySelector(`span[data-number = "${String(num)}"]`);
+//   const mySpanParent = mySpan.closest('.box');
+//   const mySpanParentDataValue = mySpanParent.dataset.section;
+//   return mySpanParentData;
+// };
+// ------------------минимальная запись---------------
 export const getSection = num => {
-  const mySpan = document.querySelector(`span[data-number = "${String(num)}"]`);
-  const mySpanParent = mySpan.closest('.box');
-  const mySpanParentData = mySpanParent.dataset.section;
-  return mySpanParentData;
+  const divIs = document.querySelector(`span[data-number = "${String(num)}"]`).closest('.box')
+    .dataset.section;
+  return divIs;
 };
 
 // getSection(4);
