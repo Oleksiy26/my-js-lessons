e9abfd2a55974e59c766df35048dc25863368de8
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-12 05:45:40 +0200

    one time click

--- Lesson22-handle/single-handling-of-the-click-event-on-the-button/index.js
+++ Lesson22-handle/single-handling-of-the-click-event-on-the-button/index.js
@@ -1,17 +1,8 @@
-// algorithm
+const singleClick = document.querySelector('.single-use-btn');
 
-// 1. выбираем все елементы на странице с классом name
-// 2. Полученный объект Nodelist преобразуем в массив с помощью Array.from
-// 3. Используя метод массива map и метод dataset итерируем массив
-// ,возводим в квадрат заначение data атрибута, создаем новый data атрибут
-// присваиваем возведенное в квадрат заначение в новый data атрибут
-
-export const squaredNumbers = () => {
-  const myItems = document.querySelectorAll('.number');
-  Array.from(myItems).map(elem => {
-    const squaredValue = elem.dataset.number ** 2;
-    elem.dataset.squaredNumber = squaredValue;
-    return elem;
-  });
+const oneTimeClick = () => {
+  console.log('clicked');
+  singleClick.removeEventListener('click', oneTimeClick);
 };
-// squaredNumbers();
+
+singleClick.addEventListener('click', oneTimeClick);
