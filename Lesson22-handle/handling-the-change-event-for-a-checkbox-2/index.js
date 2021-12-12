1ba7f555efac0dba8d505f214905f7655182af83
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-12 17:46:19 +0200

    checkbox2

--- Lesson22-handle/handling-the-change-event-for-a-checkbox-2/index.js
+++ Lesson22-handle/handling-the-change-event-for-a-checkbox-2/index.js
@@ -1,19 +1,7 @@
-export function finishList() {
-  const parent = document.querySelector('.list');
-  const child = document.querySelector('.special');
-  const missedItem1 = document.createElement('li');
-  const missedItem4 = document.createElement('li');
-  const missedItem6 = document.createElement('li');
-  const missedItem8 = document.createElement('li');
-  missedItem1.textContent = '1';
-  missedItem4.textContent = '4';
-  missedItem6.textContent = '6';
-  missedItem8.textContent = '8';
+const checkboxInput = document.querySelector('.task-status');
 
-  parent.prepend(missedItem1);
-  parent.append(missedItem8);
-  child.before(missedItem4);
-  child.after(missedItem6);
-}
+const onChange = event => {
+  console.log(event.target.checked);
+};
 
-// finishList();
+checkboxInput.addEventListener('change', onChange);
