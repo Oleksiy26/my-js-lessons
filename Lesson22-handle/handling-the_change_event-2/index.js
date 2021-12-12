b88a59b16a036ec651a080c23e8c9844557a02c2
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-12 17:36:20 +0200

    change2

--- Lesson22-handle/handling-the_change_event-2/index.js
+++ Lesson22-handle/handling-the_change_event-2/index.js
@@ -1,16 +1,7 @@
-// export const getSection = num => {
-//   const mySpan = document.querySelector(`span[data-number = "${String(num)}"]`);
-//   const mySpanParent = mySpan.closest('.box');
-//   const mySpanParentDataValue = mySpanParent.dataset.section;
-//   return mySpanParentData;
-// };
-// ------------------минимальная запись---------------
-export const getSection = num => {
-  const divIs = document.querySelector(`span[data-number = "${String(num)}"]`).closest('.box')
-    .dataset.section;
-  return divIs;
+const inputValue = document.querySelector('.text-input');
+
+const handleChange = event => {
+  console.log(event.target.value);
 };
 
-// getSection(4);
-
-// console.log(getSection(4));
+inputValue.addEventListener('change', handleChange);
