f848155dda3b09b185f198016e98eb5aaf9c2747
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-16 14:19:34 +0200

    todo

--- Lesson23-events-forms-delegation/todo_list-create_update-task/index.js
+++ Lesson23-events-forms-delegation/todo_list-create_update-task/index.js
@@ -1,15 +1,16 @@
 const tasks = [
-  { id: '1', text: 'Buy milk', done: false },
-  { id: '2', text: 'Pick up Tom from airport', done: false },
-  { id: '3', text: 'Visit party', done: false },
-  { id: '4', text: 'Visit doctor', done: true },
-  { id: '5', text: 'Buy meat', done: true },
+  { id: 1, text: 'Buy milk', done: false },
+  { id: 2, text: 'Pick up Tom from airport', done: false },
+  { id: 3, text: 'Visit party', done: false },
+  { id: 4, text: 'Visit doctor', done: true },
+  { id: 5, text: 'Buy meat', done: true },
 ];
 
 const listElem = document.querySelector('.list');
 const createNew = document.querySelector('.create-task-btn');
 
 const renderTasks = tasksList => {
+  listElem.innerHTML = '';
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
     .map(({ text, done, id }) => {
@@ -33,37 +34,65 @@ const renderTasks = tasksList => {
 
 renderTasks(tasks);
 
+// const onCheked = event => {
+//   const isCheckbox = event.target.classList.contains('list__item-checkbox');
+//   if (!isCheckbox) {
+//     return;
+//   }
+//   const checkedItem = event.target.dataset.id;
+//   tasks
+//     .filter(elem => elem.id === Number(checkedItem))
+//     .map(key => {
+//       key.done = event.target.checked;
+//       return key.done;
+//     });
+
+//   renderTasks(tasks);
+// };
+
+// const hendleClick = () => {
+//   const inputText = document.querySelector('.task-input');
+//   const newEvent = {
+//     id: String(Math.floor(Math.random() * (20 - 6)) + 6),
+//     text: inputText.value,
+//     done: false,
+//   };
+//   if (inputText.value && isNaN(inputText.value)) {
+//     tasks.push(newEvent);
+//   }
+//   inputText.value = '';
+//   listElem.innerHTML = '';
+//   renderTasks(tasks);
+// };
+
+// -------------------refactored-------------------
+
 const onCheked = event => {
   const isCheckbox = event.target.classList.contains('list__item-checkbox');
   if (!isCheckbox) {
     return;
   }
-  const checkedItem = event.target.dataset.id;
-  tasks
-    .filter(elem => elem.id === String(checkedItem))
-    .map(key => {
-      key.done = event.target.checked;
-      return key.done;
-    });
-  listElem.innerHTML = '';
+  const task = tasks.find(el => el.id === Number(event.target.dataset.id));
+  task.done = event.target.checked;
+  console.log(task);
   renderTasks(tasks);
 };
 
-const hendleClick = () => {
+const createNewTaskHendler = () => {
   const inputText = document.querySelector('.task-input');
-
+  if (inputText.value === '' || !isNaN(inputText.value)) {
+    return;
+  }
   const newEvent = {
-    id: String(Math.floor(Math.random() * (20 - 6)) + 6),
+    id: tasks.length + 1,
     text: inputText.value,
     done: false,
   };
-  if (inputText.value && isNaN(inputText.value)) {
-    tasks.push(newEvent);
-  }
+  tasks.push(newEvent);
+
   inputText.value = '';
-  listElem.innerHTML = '';
   renderTasks(tasks);
 };
 
 listElem.addEventListener('click', onCheked);
-createNew.addEventListener('click', hendleClick);
+createNew.addEventListener('click', createNewTaskHendler);
