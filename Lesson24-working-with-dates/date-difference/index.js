ee060858c55c48d275ef8f6a575301eece806cfc
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-16 04:18:37 +0200

    difference

--- Lesson24-working-with-dates/date-difference/index.js
+++ Lesson24-working-with-dates/date-difference/index.js
@@ -1,12 +1,10 @@
 export const getDiff = (startDate, endDate) => {
-  const result = endDate - startDate;
-
   // const days = Math.floor(result / (1000 * 60 * 60 * 24));
   // const hours = Math.floor((result / (1000 * 60 * 60)) % 24);
   // const minutes = Math.floor((result / (1000 * 60)) % 60);
   // const seconds = Math.floor((result / 1000) % 60);
   // const diff = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-  return result;
+  return endDate - startDate;
 };
 // console.log(getDiff(new Date(), new Date(2022, 0, 26)));
