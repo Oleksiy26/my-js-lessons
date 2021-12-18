06750fb2bd287c340932aeaf36346770911c2ea2
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-18 19:40:13 +0200

    local storage

--- Lesson27-local-storage/getting-all-localStorage-data/index.js
+++ Lesson27-local-storage/getting-all-localStorage-data/index.js
@@ -5,9 +5,15 @@ localStorage.setItem('age', JSON.stringify(17));
 
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
     return {
       ...acc,
-      [key]: JSON.parse(value),
+      [key]: newValue,
     };
   }, {});
 };
