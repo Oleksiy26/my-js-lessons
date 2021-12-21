6f2c361ac13cb9e2af32e7a27be5a7a26b0a1538
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-22 00:36:05 +0200

    load photo

---
+++ Lesson29-asynchronous-code/load-image/index.js
@@ -0,0 +1,31 @@
+const addImage = (imgSrc, callback) => {
+  const imgElem = document.createElement('img');
+  imgElem.setAttribute('alt', 'My Photo');
+  imgElem.src = imgSrc;
+  const container = document.querySelector('.page');
+  container.append(imgElem);
+
+  const onImgLoaded = () => {
+    const { width, height } = imgElem;
+    callback(null, { width, height });
+  };
+
+  imgElem.addEventListener('load', onImgLoaded);
+  imgElem.addEventListener('error', () => callback('Image  load is failed'));
+};
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
+
+const onImageLoaded = (error, imgElem) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { width, height } = imgElem;
+  const sizeElem = document.querySelector('.image-size');
+
+  sizeElem.textContent = `${width} x ${height}`;
+};
+
+addImage(imgSrc, onImageLoaded);
---
+++ Lesson29-asynchronous-code/write-with-interval/index.js
@@ -0,0 +1,6 @@
+export const mult = a => b => a * b;
+export const twice = mult(2);
+export const triple = mult(3);
+
+const result = triple(9);
+console.log(result);
