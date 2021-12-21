a34e2e396df5a8ee6080f5ca41f1156d9f11e9b3
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-22 01:18:21 +0200

    load photo

--- Lesson29-asynchronous-code/load-image/index.js
+++ Lesson29-asynchronous-code/load-image/index.js
@@ -1,11 +1,11 @@
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
-  imgElem.setAttribute('alt', 'My Photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
 
   const onImageLoaded = () => {
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
     callback(null, imgElem);
   };
 
