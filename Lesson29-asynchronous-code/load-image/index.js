68801cb174962ffa3a24b0d63adf8436a2bbdc37
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-22 01:27:42 +0200

    load photo

--- Lesson29-asynchronous-code/load-image/index.js
+++ Lesson29-asynchronous-code/load-image/index.js
@@ -1,26 +1,25 @@
 export const addImage = (imgSrc, callback) => {
   const imgElem = document.createElement('img');
 
-  imgElem.setAttribute('alt', 'My Photo');
-  imgElem.src = imgSrc;
-  const containerElem = document.querySelector('.page');
-  containerElem.append(imgElem);
+  const onImageLoaded = () => {
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+    callback(null, imgElem);
+  };
 
-  const { width, height } = imgElem;
-
-  imgElem.addEventListener('load', () => {
-    callback(null, { width, height });
-  });
+  imgElem.addEventListener('load', onImageLoaded);
   imgElem.addEventListener('error', () => callback('Image  load is failed'));
 };
 
-const onImageLoaded = (error, imgElem) => {
+const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  const { width, height } = imgElem;
+  const { width, height } = data;
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
