198031250e088b02a9281f46f72a619e82628129
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-21 02:08:22 +0200

    composition

---
+++ Lesson28/composition/index.js
@@ -0,0 +1,13 @@
+const add2 = value => value + 2;
+const square = value => value ** 2;
+const half = value => value / 2;
+
+const compose =
+  (...funcs) =>
+  value => {
+    return funcs.reduce((acc, func) => func(acc), value);
+  };
+
+const superFunc = compose(add2, square, half);
+
+console.log(superFunc(8));
--- Lesson28/composition/src/scripts/todoList.js
+++
@@ -1,9 +0,0 @@
-import { createNewTaskHendler } from './createTask.js';
-import { onToggleTask } from './updateTask.js';
-
-export const initToDoListHendlers = () => {
-  const createNew = document.querySelector('.create-task-btn');
-  createNew.addEventListener('click', createNewTaskHendler);
-  const toDolistElem = document.querySelector('.list');
-  toDolistElem.addEventListener('click', onToggleTask);
-};
