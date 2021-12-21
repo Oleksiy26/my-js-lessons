54b337b094fc0b67d7942e8564059ae7b04292e4
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-22 01:33:56 +0200

    load photo

--- Lesson29-asynchronous-code/load-image/index.js
+++ Lesson29-asynchronous-code/load-image/index.js
@@ -11,22 +11,23 @@ export const addImage = (imgSrc, callback) => {
   };
 
   imgElem.addEventListener('load', onImageLoaded);
-  imgElem.addEventListener('error', () => callback('Image  load is failed'));
+
+  imgElem.addEventListener('error', () => callback('Image load failed'));
 };
+
+const imgSrc =
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
 const onImageLoaded = (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-
   const { width, height } = data;
+
   const sizeElem = document.querySelector('.image-size');
 
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(
-  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
-  onImageLoaded,
-);
+addImage(imgSrc, onImageLoaded);
