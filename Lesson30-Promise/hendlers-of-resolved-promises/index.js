840b1552caaa60c288d983b4f42847356fc6435f
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 00:02:07 +0200

    resolved

--- Lesson30-Promise/hendlers-of-resolved-promises/index.js
+++ Lesson30-Promise/hendlers-of-resolved-promises/index.js
@@ -1,15 +1,34 @@
-export const pinger = (num, period) => {
-  let i = num;
-  console.log('Ping');
-  const interval = setInterval(() => {
-    if (--i > 0) {
-      console.log('Ping');
-    } else {
-      clearInterval(interval);
-    }
-  }, period);
-};
+/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
+const userDataPromise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve({ name: 'Tom', age: 17 });
+  }, 1000);
+});
 
-console.log(pinger(5, 1000));
+/*
+ * Выведите в консоль переменную userDataPromise
+ * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ */
+console.log(userDataPromise);
+/* ...code here */
 
-// console.log(pinger(7, 150));
+/*
+ * Выведите в консоль переменную userData в обработчике промиса
+ * Ответьте себе на вопрос, что находится в переменной userData?
+ */
+userDataPromise.then(function onSuccess(userData) {
+  /* ...code here */
+  console.log(userData);
+});
+
+/*
+ * подпишитесь на успешное выполнение промиса userDataPromise
+ * используйте метод .then
+ * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
+ * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ */
+
+userDataPromise.then(function onSuccess({ name, age }) {
+  /* ...code here */
+  console.log(`My name is ${name}. I am ${age} years old`);
+});
