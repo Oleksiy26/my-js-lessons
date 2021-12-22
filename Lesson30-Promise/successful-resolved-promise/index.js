10fcf7b0262e207ad5fd1a240e30e808155feb73
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 00:05:01 +0200

    successful-resolved-promise

--- Lesson30-Promise/successful-resolved-promise/index.js
+++ Lesson30-Promise/successful-resolved-promise/index.js
@@ -1,21 +1,34 @@
-export const printing = () => {
-  console.log(1);
-
-  setTimeout(function () {
-    console.log(5);
+/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
+const userDataPromise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve({ name: 'Tom', age: 17 });
   }, 1000);
+});
 
-  console.log(2);
+/*
+ * Выведите в консоль переменную userDataPromise
+ * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ */
+console.log(userDataPromise);
+/* ...code here */
 
-  setTimeout(function () {
-    console.log(4);
-  }, 0);
+/*
+ * Выведите в консоль переменную userData в обработчике промиса
+ * Ответьте себе на вопрос, что находится в переменной userData?
+ */
+userDataPromise.then(function onSuccess(userData) {
+  /* ...code here */
+  console.log(userData);
+});
 
-  setTimeout(function () {
-    console.log(6);
-  }, 2000);
+/*
+ * подпишитесь на успешное выполнение промиса userDataPromise
+ * используйте метод .then
+ * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
+ * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ */
 
-  console.log(3);
-};
-
-printing();
+userDataPromise.then(function onSuccess({ name, age }) {
+  /* ...code here */
+  console.log(`My name is ${name}. I am ${age} years old`);
+});
