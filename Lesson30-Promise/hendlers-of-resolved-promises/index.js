ee572db83d9b32428ff3aff016f46d9b9e8ccace
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 00:33:02 +0200

    resolved

--- Lesson30-Promise/hendlers-of-resolved-promises/index.js
+++ Lesson30-Promise/hendlers-of-resolved-promises/index.js
@@ -1,34 +1,32 @@
-/* здесь мы создаем промис, который резолвит объект { name: 'Tom', age: 17 } через 1 секунду */
-const userDataPromise = new Promise(resolve => {
-  setTimeout(() => {
-    resolve({ name: 'Tom', age: 17 });
-  }, 1000);
+/* ===> 1 <=== */
+
+/*
+ * successPromise должен зарезолвить число 67
+ * Ответьте себе на вопрос, какой тип данных имеет переменная successPromise
+ */
+
+const successPromise = new Promise(resolve => {
+  /* ...code here */
+  resolve(67);
 });
 
 /*
- * Выведите в консоль переменную userDataPromise
- * Ответьте себе на вопрос, какой тип данных имеет переменная userDataPromise?
+ * допишите обработчик успешного промиса (аргументы и тело ф-ции onSuccess)
+ * чтобы в консоль вывелся квадрат числа, которое резолвиться из successPromise
  */
-console.log(userDataPromise);
-/* ...code here */
 
-/*
- * Выведите в консоль переменную userData в обработчике промиса
- * Ответьте себе на вопрос, что находится в переменной userData?
- */
-userDataPromise.then(function onSuccess(userData) {
+successPromise.then(function onSuccess(number) {
   /* ...code here */
-  console.log(userData);
+  console.log(number ** 2);
 });
 
 /*
- * подпишитесь на успешное выполнение промиса userDataPromise
- * используйте метод .then
- * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
- * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ * в обработчике ошибок промиса (ф-ция onError внутри .catch()) выведите в консоль текст 'I am an error'
+ * текст вывелся в консоль?
+ * подумайте почему
+ * потому что reject is not defined!
  */
-
-userDataPromise.then(function onSuccess({ name, age }) {
+successPromise.catch(function onError() {
   /* ...code here */
-  console.log(`My name is ${name}. I am ${age} years old`);
+  console.log('I am an error');
 });
