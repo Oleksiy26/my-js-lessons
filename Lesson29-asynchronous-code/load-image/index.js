55609d73b8a2dace8b5ba8353a5ae5042f4fbfb2
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-22 02:05:29 +0200

    load photo

--- Lesson29-asynchronous-code/load-image/index.js
+++ Lesson29-asynchronous-code/load-image/index.js
@@ -12,18 +12,18 @@ export const addImage = (imgSrc, callback) => {
 
   imgElem.addEventListener('load', onImageLoaded);
 
-  imgElem.addEventListener('error', () => callback('Image load failed'));
+  imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
 const imgSrc =
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const onImageLoaded = (error, data) => {
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = data;
+  const { width, height } = imgElem;
 
   const sizeElem = document.querySelector('.image-size');
 
