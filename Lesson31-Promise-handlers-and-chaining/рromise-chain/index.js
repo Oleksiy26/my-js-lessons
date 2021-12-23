29532cf65d566628415ce0b23812cc26195ab65b
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 02:43:24 +0200

    request data

---
+++ Lesson31-Promise-handlers-and-chaining/рromise-chain/index.js
@@ -0,0 +1,26 @@
+export const addImage = imgSrc => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    imgElem.addEventListener('load', () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    });
+    imgElem.addEventListener('error', () => reject(new Error('Image load is failed...')));
+  });
+  return p;
+};
+
+// const resultPromise = addImage(imgSrc);
+
+// resultPromise
+//   .then(data => {
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
