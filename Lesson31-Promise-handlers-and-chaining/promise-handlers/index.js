ae050598ac545e3051abf655f3432360db6cec7c
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 02:46:42 +0200

    request data

--- Lesson31-Promise-handlers-and-chaining/promise-handlers/index.js
+++ Lesson31-Promise-handlers-and-chaining/promise-handlers/index.js
@@ -6,7 +6,7 @@ export const requestUserData = userId => {
       }, 500);
     } else {
       setTimeout(() => {
-        resolve({ name: 'John', age: 17, email: `${userId}`, userId });
+        resolve({ name: 'John', age: 17, email: `${userId}`, userId: 'userid777' });
       }, 1000);
     }
   });
