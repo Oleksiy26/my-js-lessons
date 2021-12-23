3fba4a5857eef90d85f84a3f6d878db879688616
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-23 03:38:06 +0200

    prom chain

--- Lesson31-Promise-handlers-and-chaining/рromise-chain/index.js
+++ Lesson31-Promise-handlers-and-chaining/рromise-chain/index.js
@@ -1,26 +1,24 @@
-export const addImage = imgSrc => {
-  const p = new Promise((resolve, reject) => {
-    const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
+export const asyncCalculator = num =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      console.log(`Initial value: ${num}`);
+      resolve(num);
+    }, 500);
+  }).then(value =>
+    new Promise(resolve => {
+      setTimeout(() => {
+        const squared = value ** 2;
+        console.log(`Squared value: ${squared}`);
+        resolve(squared);
+      }, 500);
+    }).then(
+      value =>
+        new Promise(resolve => {
+          const doubled = value * 2;
+          console.log(`Doubled value: ${doubled}`);
+          resolve(doubled);
+        }),
+    ),
+  );
 
-    imgElem.addEventListener('load', () => {
-      const { width, height } = imgElem;
-      resolve({ width, height });
-    });
-    imgElem.addEventListener('error', () => reject(new Error('Image load is failed...')));
-  });
-  return p;
-};
-
-// const resultPromise = addImage(imgSrc);
-
-// resultPromise
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+// asyncCalculator(2);
