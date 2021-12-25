c0176ce2957a9ab0f35e54def1e96d90aaef0e4a
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-25 02:48:31 +0200

    promise race

--- Lesson32-promise-static-methods/promise-race/index.js
+++ Lesson32-promise-static-methods/promise-race/index.js
@@ -1,10 +1,19 @@
-export const delay = milliseconds =>
+const getRandomNum = (from, to) => from + Math.random() * (to - from);
+
+const request = url =>
   new Promise(resolve => {
+    const timeout = getRandomNum(1000, 3000);
     setTimeout(() => {
-      resolve();
-    }, milliseconds);
-  }).then(() => {
-    console.log('Done');
+      resolve({ userData: { name: 'Tom', age: 17 }, source: url });
+    }, timeout);
   });
 
-// delay(3000);
+const servers = ['https://server.com/us/', 'https://server.com/eu/', 'https://server.com/au/'];
+
+const getUserASAP = userId => {
+  const userUrls = servers.map(serverUrl => `${serverUrl}${userId}`);
+  const requests = userUrls.map(userUrl => request(userUrl));
+  return Promise.race(requests);
+};
+
+// getUserASAP('usrer-12').then(url => console.log(url));
