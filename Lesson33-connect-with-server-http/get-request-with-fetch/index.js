3e6d23a92cff22e4bb7cf9b94271cc3ce02ca517
Oleksii Lubianyi <alexl30.al@gmail.com>
2021-12-28 05:19:40 +0200

    fetch HTTPS

--- Lesson33-connect-with-server-http/get-request-with-fetch/index.js
+++ Lesson33-connect-with-server-http/get-request-with-fetch/index.js
@@ -4,9 +4,9 @@ const userNameElem = document.querySelector('.user__name');
 
 const userLocationElem = document.querySelector('.user__location');
 
-// const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
+const defaultAvatar = 'https://avatars3.githubusercontent.com/u10001';
 
-// userAvatarElem.src = defaultAvatar;
+userAvatarElem.src = defaultAvatar;
 
 const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
