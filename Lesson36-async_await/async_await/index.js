9c962e38fb4fe703a7236726bcfe20ededc41e03
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-01-13 01:32:53 +0200

    async await

--- Lesson36-async_await/async_await/index.js
+++ Lesson36-async_await/async_await/index.js
@@ -1,16 +1,16 @@
 export const fetchUser = async userId => {
   try {
-    const response = await fetch(`https://api.github.com/users/${userId}`);
-    if (!response.ok) {
+    const response = fetch(`https://api.github.com/users/${userId}`);
+    if (response.status === 404) {
       return null;
     }
     const userData = await response.json();
     return userData;
   } catch (err) {
-    throw new Error('Failed fetch');
+    throw new Error('Failed to fetch user');
   }
 };
 
-// fetchUser('github')
-//   .then(userData => console.log(userData))
-//   .catch(err => alert(err.message));
+fetchUser('github')
+  .then(userData => console.log(userData))
+  .catch(err => alert(err.message));
